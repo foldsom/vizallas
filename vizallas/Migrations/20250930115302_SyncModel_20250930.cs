f1e4115af4bf49bf9ec7dc4174dc9836de6fb808
foldi.soma.vajk <ugylokuka@gmail.com>
2025-09-30 13:54:38 +0200

    eddig kesz

---
+++ vizallas/Migrations/20250930115302_SyncModel_20250930.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vizallas.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModel_20250930 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VizallaS",
+                table: "Vizallas",
+                newName: "Vizallas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Vizallas",
+                table: "Vizallas",
+                newName: "VizallaS");
+        }
+    }
+}
