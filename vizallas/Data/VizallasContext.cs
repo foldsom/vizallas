4908e03553dd9efa0acfb82cd346383fc9be9d78
foldi.soma.vajk <ugylokuka@gmail.com>
2025-09-30 13:05:01 +0200

    sdij

---
+++ vizallas/Data/VizallasContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace vizallas.Data
+{
+    public class VizallasContext : DbContext
+    {
+        public VizallasContext (DbContextOptions<VizallasContext> options)
+            : base(options) { }
+        public DbSet<vizallas.Models.Vizallas> Vizallas { get; set; } = default!;
+
+    }
+}
