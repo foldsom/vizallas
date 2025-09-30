e01e76e2818c09f9a07d0adfb02cc953b282040d
foldi.soma.vajk <foldi.soma.vajk@BURG-2E05A-18>
2025-09-30 12:56:11 +0200

    Add project files.

---
+++ vizallas/Models/Vizallas.cs
@@ -0,0 +1,12 @@
+﻿namespace vizallas.Models
+{
+    public class Vizallas
+    {
+        public int Id { get; set; }
+        public DateTime Datum { get; set; }
+        public int VizallaS { get; set; }
+        public string Varos { get; set; } = string.Empty;
+        public string Folyo { get; set; } = string.Empty;
+
+    }
+}
